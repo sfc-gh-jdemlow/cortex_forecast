3e5ed7bdf6d5f11b62d0c2e85b3761eb280b91cf
Jeremy Demlow <157405973+sfc-gh-jdemlow@users.noreply.github.com>
2024-08-20 18:39:55 -0700

    inital push working on docs and then needing to work through taxi example as it works with series and additional variables

---
+++ cortex_forecast/queries.py
@@ -0,0 +1,11 @@
+from forecast.connection import SnowparkConnection
+
+def run_query(query):
+    session = SnowparkConnection().get_session()
+    df = session.sql(query).to_pandas() if session else None
+    return df
+
+def run_command(query):
+    session = SnowparkConnection().get_session()
+    df = session.sql(query).collect() if session else None
+    return df
